f3c5dfee1925e2a9f37b9e72430e4364bb943052
fergaloconnor <117333926@umail.ucc.ie>
2021-01-28 18:37:38 +0000

    -Version3.09, added code to find target joint in myHealthActivity

--- app/src/main/java/com/example/MyBleeds/myHealthActivity.java
+++ app/src/main/java/com/example/MyBleeds/myHealthActivity.java
@@ -2,29 +2,13 @@ package com.example.MyBleeds;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.SeekBar;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -33,17 +17,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class myHealthActivity extends AppCompatActivity {
@@ -69,13 +54,49 @@ public class myHealthActivity extends AppCompatActivity {
 
     BottomNavigationView itemSelectedListener;
 
-    TextView txtBleedAmount;
+    TextView txtBleedAmount,txtTarget;
 
     int countBleeds = 0;
 
     //Sets Dates for now and for 6 months
     String currentDate = new SimpleDateFormat("yyyy/MM/dd", Locale.getDefault()).format(new Date());
     String SixDates = (LocalDate.now().minusMonths(6).format(DateTimeFormatter.ofPattern("yyyy/MM/dd")));
+
+
+
+    //Array to hold the ints.
+    List<Integer> listofBleeds = new ArrayList<Integer>();
+
+    //Creating ints to count the amount of a certain bleed location
+            int abdominal;
+         int Ankle;
+       int Arm;
+      int Back;
+        int Buttock;
+        int Calf;
+  int DentalProcedure;
+        int Ear;
+        int Elbow;
+       int Eye;
+      int Face;
+       int Foot;
+       int Gums;
+     int Hand;
+       int Head;
+        int Hip;
+    int Knee;
+       int Neck;
+     int Nose;
+        int Shoulder;
+       int Thigh;
+     int Toe;
+        int Tongue ;
+       int Wrist;
+       int Other;
+
+
+
+
 
 
     @Override
@@ -87,6 +108,9 @@ public class myHealthActivity extends AppCompatActivity {
 
         itemSelectedListener = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         txtBleedAmount = (TextView) findViewById(R.id.txtBleedAmount);
+        txtTarget = (TextView) findViewById(R.id.txtTarget);
+
+
 
 
         Intent intent = getIntent();
@@ -98,11 +122,6 @@ public class myHealthActivity extends AppCompatActivity {
         String id = intent.getStringExtra(PatientSettingsActivity.PATIENT_ID);
         databaseBleeds = FirebaseDatabase.getInstance().getReference("bleeds").child(id);
         query = databaseBleeds.child(id);
-
-
-
-
-
 
 
         //Bottom navigation switch case to decide location based upon selected item
@@ -160,8 +179,6 @@ public class myHealthActivity extends AppCompatActivity {
             }
 
 
-
-
             }
 
             @Override
@@ -172,6 +189,172 @@ public class myHealthActivity extends AppCompatActivity {
 
 
 
+        //Finds the count of all bleeds for each bleed location, used to find the target joint.
+        databaseBleeds.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot snapShot :dataSnapshot.getChildren()) {
+
+                    switch (snapShot.child("bleedName").getValue(String.class)) {
+                        case "Abdominal Bleed":
+                            ++abdominal;
+                            break;
+                        case "Ankle":
+                            ++Ankle;
+                            break;
+                        case "Arm":
+                            ++Arm;
+                            break;
+                        case "Back":
+                            ++Back;
+                            break;
+                        case "Buttock":
+                            ++Buttock;
+                            break;
+                        case "Calf":
+                            ++Calf;
+                            break;
+                        case "Dental Procedure":
+                            ++DentalProcedure;
+                            break;
+                        case "Ear":
+                            ++Ear;
+                            break;
+                        case "Elbow":
+                            ++Elbow;
+                            break;
+                        case "Eye":
+                            ++Eye;
+                            break;
+                        case "Face":
+                            ++Face;
+                            break;
+                        case "Foot":
+                            ++Foot;
+                            break;
+                        case "Gums":
+                            ++Gums;
+                            break;
+                        case "Hand":
+                            ++Hand;
+                            break;
+                        case "Head":
+                            ++Head;
+                            break;
+                        case "Hip":
+                            ++Hip;
+                            break;
+                        case "Knee":
+                            ++Knee;
+                            break;
+                        case "Neck":
+                            ++Neck;
+                            break;
+                        case "Nose":
+                            ++Nose;
+                            break;
+                        case "Shoulder":
+                            ++Shoulder;
+                            break;
+                        case "Thigh":
+                            ++Thigh;
+                            break;
+                        case "Toe":
+                            ++Toe;
+                            break;
+                        case "Tongue":
+                            ++Tongue;
+                            break;
+                        case "Wrist":
+                            ++Wrist;
+                            break;
+                        case "Other":
+                            ++Other;
+                            break;
+
+                    }
+
+                    /*
+                    listofBleeds.add(abdominal);
+                    listofBleeds.add(Ankle);
+                    listofBleeds.add(Arm);
+                    listofBleeds.add(Back);
+                    listofBleeds.add(Buttock);
+                    listofBleeds.add(Calf);
+                    listofBleeds.add(DentalProcedure);
+                    listofBleeds.add(Ear);
+                    listofBleeds.add(Elbow);
+                    listofBleeds.add(Eye);
+                    listofBleeds.add(Face);
+                    listofBleeds.add(Foot);
+                    listofBleeds.add(Gums);
+                    listofBleeds.add(Head);
+                    listofBleeds.add(Hip);
+                    listofBleeds.add(Knee);
+                    listofBleeds.add(Neck);
+                    listofBleeds.add(Nose);
+                    listofBleeds.add(Shoulder);
+                    listofBleeds.add(Thigh);
+                    listofBleeds.add(Tongue);
+                    listofBleeds.add(Toe);
+                    listofBleeds.add(Wrist);
+                    listofBleeds.add(Other);
+*/
+
+                    // Integer max = Collections.max(listofBleeds);
+                    // txtTarget.setText(Integer.toString(max));
+
+
+                    //A hash map that has all bleed locations no. of bleeds there.
+                    HashMap<String , Integer> topBleeds = new HashMap<String , Integer>();
+
+                    topBleeds.put( "Abdominal Bleed",abdominal);
+                    topBleeds.put("Ankle", Ankle);
+                    topBleeds.put( "Arm",Arm);
+                    topBleeds.put( "Abdominal Bleed", Back);
+                    topBleeds.put( "Buttock",Buttock);
+                    topBleeds.put( "Calf",Calf);
+                    topBleeds.put( "Dental",DentalProcedure);
+                    topBleeds.put( "Ear",Ear);
+                    topBleeds.put( "Elbow",Elbow);
+                    topBleeds.put("Eye",Eye);
+                    topBleeds.put( "Face",Face);
+                    topBleeds.put( "Foot",Foot);
+                    topBleeds.put( "Gums",Gums);
+                    topBleeds.put( "Head",Head);
+                    topBleeds.put( "Hip",Hip);
+                    topBleeds.put( "Knee",Knee);
+                    topBleeds.put("Neck",Neck);
+                    topBleeds.put("Nose", Nose);
+                    topBleeds.put( "Thigh",Thigh);
+                    topBleeds.put( "Tongue",Tongue);
+                    topBleeds.put( "Toe",Toe);
+                    topBleeds.put( "Wrist",Wrist);
+                    topBleeds.put( "Other",Other);
+
+                    //Code to find the max value from the created hash map.
+                    //https://stackoverflow.com/questions/5911174/finding-key-associated-with-max-value-in-a-java-map
+                    Map.Entry<String, Integer> maxEntry = null;
+                    for (Map.Entry<String, Integer> entry : topBleeds.entrySet()) {
+                        if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+                            maxEntry = entry;
+                            String TargetJoint = maxEntry.toString();
+                            txtTarget.setText(TargetJoint);
+
+                        }
+                        String maxKey = Collections.max(topBleeds.keySet());
+
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
     }
@@ -184,5 +367,7 @@ public class myHealthActivity extends AppCompatActivity {
 
 
     }
+
+
 
 }
